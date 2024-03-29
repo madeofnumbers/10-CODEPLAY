e96b597cf3582f6bb6e82618db906e136e024764
Simon <simon@madeofnumbers.com>
2024-03-29 12:07:19 +0100

    Update demo.js

--- demo/demo.js
+++ demo/demo.js
@@ -15,9 +15,12 @@ function addCodeplayExamplesDemo(domElement) {
   let refreshExamples = [];
 
   // ===== notes on 10 / CODEPLAY =====
-  addHtml("h1", "10 / CODEPLAY : write code to solve puzzles");
-  addHtml("p", "About : example code to interact with <a href='https://madeofnumbers.com/10/' target='_blank'>madeofnumbers/10</a>, a game where the player slides numbers into each other to make 10s. Once all the numbers have been combined, the player has solved the puzzle.")
-  addHtml("p", "The <strong>Mon10</strong> object from <strong>https://madeofnumbers.com/10/mon10-latest.min.js</strong> provides the following methods.");
+  addElement("h1", "10 / CODEPLAY : write code to solve puzzles");
+  addElement("div", "This examples demo is not great on small&nbsp;screens. We would recommend you try it out on a larger&nbsp;display.", "note-small-screens");
+  addElement("p", "<strong>GitHub : <a href='https://github.com/madeofnumbers/10-codeplay/' target='_blank'>https://github.com/madeofnumbers/10-codeplay/</a></strong>");
+  addElement("p", "<strong>About :</strong> example code to interact with <a href='https://madeofnumbers.com/10/' target='_blank'>madeofnumbers/10</a>, a game where the player slides numbers into each other to make 10s. Once all the numbers have been combined, the player has solved the&nbsp;puzzle.");
+  addElement("p", "<a href='#example-grid-summary'>Example : gridSummary()</a><br><a href='#example-random-moves'>Example : randomMoves()</a>")
+  addElement("p", "The <strong>Mon10</strong> object from <strong>https://madeofnumbers.com/10/mon10-latest.min.js</strong> provides the following&nbsp;methods :");
 
   // ===== example mon10.title() =====
   addExample({
@@ -159,17 +162,16 @@ function addCodeplayExamplesDemo(domElement) {
   });
 
   // ===== notes on Mon10 object =====
-  addHtml("h2", "Notes :");
-  addHtml("p", "<strong>mon10.getXY(x, y)</strong><br> -- <strong>x, y</strong> // (int, int) position : top-left corner of the grid is (0, 0)<br>result :<br> -- <strong>block: 1</strong> // is a non-numeric block tile<br> -- <strong>empty: 1</strong> // is empty, no tile at that position<br> -- <strong>number: <em>value</em></strong> // is a numeric tile, including <strong>number: 0</strong> which is 0<br> -- <strong>move: <em>direction</em></strong> // permitted directions : 0 = none, 1 = up, 2 = right, 3 = down, 4 = left, 5 = up/down, 6 = left/right, 7 = all<br>");
-  addHtml("p", "<strong>mon10.moveXYD(x, y, d)</strong><br> -- <strong>x, y</strong> // (int, int) position : top-left corner of the grid is (0, 0)<br> -- <strong>d</strong> // (int) direction : 1 = up, 2 = right, 3 = down, 4 = left<br>result :<br> -- <strong>x:, y:</strong> // tile moved to this new position<br> -- <strong>fail: 1</strong> // tile could not be moved<br> -- <strong>fail: 2</strong> // tile could have been moved but would result in an illegal value (below -10 or above 10)<br>");
-  addHtml("p", "<strong>mon10.loadMaker(c)</strong><br> -- <strong>c</strong> // (string) custom : for test purposes you can load puzzles created in <a href='https://madeofnumbers.com/10/maker/' target='_blank'>10 / MAKER</a><br>In a share link the parameter '<strong>c=</strong>' is the custom string - e.g. from <a href='https://madeofnumbers.com/10/?c=3MpDvCvDpM' target='_blank'>https://madeofnumbers.com/10/?c=3MpDvCvDpM</a> the custom string is&nbsp;<strong>3MpDvCvDpM</strong> - an empty or invalid string will result in the daily puzzle being loaded");
-  addHtml("hr");
-
+  addElement("p", "<strong>mon10.getXY(x, y)</strong><br> -- <strong>x, y</strong> // (int, int) position : top-left corner of the grid is (0, 0)<br>result :<br> -- <strong>block: 1</strong> // is a non-numeric block tile<br> -- <strong>empty: 1</strong> // is empty, no tile at that position<br> -- <strong>number: <em>value</em></strong> // is a numeric tile, including <strong>number: 0</strong> which is 0<br> -- <strong>move: <em>direction</em></strong> // permitted directions : 0 = none, 1 = up, 2 = right, 3 = down, 4 = left, 5 = up/down, 6 = left/right, 7 = all<br>");
+  addElement("p", "<strong>mon10.moveXYD(x, y, d)</strong><br> -- <strong>x, y</strong> // (int, int) position : top-left corner of the grid is (0, 0)<br> -- <strong>d</strong> // (int) direction : 1 = up, 2 = right, 3 = down, 4 = left<br>result :<br> -- <strong>x:, y:</strong> // tile moved to this new position<br> -- <strong>fail: 1</strong> // tile could not be moved<br> -- <strong>fail: 2</strong> // tile could have been moved but would result in an illegal value (below -10 or above 10)<br>");
+  addElement("p", "<strong>mon10.loadMaker(c)</strong><br> -- <strong>c</strong> // (string) custom : for test purposes you can load puzzles created in <a href='https://madeofnumbers.com/10/maker/' target='_blank'>10 / MAKER</a><br>In a share link the parameter '<strong>c=</strong>' is the custom string - e.g. from <a href='https://madeofnumbers.com/10/?c=3MpDvCvDpM' target='_blank'>https://madeofnumbers.com/10/?c=3MpDvCvDpM</a> the custom string is&nbsp;<strong>3MpDvCvDpM</strong> - an empty or invalid string will result in the daily puzzle being loaded");
+  
   // ===== example gridSummary() / mon10-codeplay-example-grid-summary.js =====
-  addHtml("h1", "Example : gridSummary()");
-  addHtml("p", "Uses mon10.getXY() to build a simple summary of the current grid state, including positions of tiles grouped by value");
-  addHtml("p", "Script : <a href='../examples/grid-summary/mon10-codeplay-example-grid-summary.js' target='_blank'>examples/grid-summary/mon10-codeplay-example-grid-summary.js</a>");
-  addHtml("p", "", "html-grid-summary");
+  addElement("hr", "", "example-grid-summary");
+  addElement("h1", "Example : gridSummary()");
+  addElement("p", "Uses mon10.getXY() to build a simple summary of the current grid state, including positions of tiles grouped by&nbsp;value");
+  addElement("p", "Script : <a href='../examples/grid-summary/mon10-codeplay-example-grid-summary.js' target='_blank'>examples/grid-summary/mon10-codeplay-example-grid-summary.js</a>");
+  addElement("p", "", "html-grid-summary");
   addExample({
     name: "gridSummary",
     use: () => {
@@ -178,13 +180,13 @@ function addCodeplayExamplesDemo(domElement) {
     },
     comment: "returns a summary of current grid state"
   });
-  addHtml("hr");
   
   // ===== example randomMoves() / mon10-codeplay-example-random-moves.js =====
-  addHtml("h1", "Example : randomMoves()");
-  addHtml("p", "Uses mon10.getXY(), mon10.moveXYD(), mon10.nonzero() and mon10.size() to make entirely random moves with almost no chance of solving a puzzle");
-  addHtml("p", "Script : <a href='../examples/random-moves/mon10-codeplay-example-random-moves.js' target='_blank'>examples/random-moves/mon10-codeplay-example-random-moves.js</a>");
-  addHtml("p", "", "html-random-moves-log");
+  addElement("hr", "", "example-random-moves");
+  addElement("h1", "Example : randomMoves()");
+  addElement("p", "Uses mon10.getXY(), mon10.moveXYD(), mon10.nonzero() and mon10.size() to make entirely random moves with almost no chance of solving&nbsp;a&nbsp;puzzle");
+  addElement("p", "Script : <a href='../examples/random-moves/mon10-codeplay-example-random-moves.js' target='_blank'>examples/random-moves/mon10-codeplay-example-random-moves.js</a>");
+  addElement("p", "", "html-random-moves-log");
   addExample({
     name: "randomMoves",
     params: [{
@@ -213,15 +215,15 @@ function addCodeplayExamplesDemo(domElement) {
   });
 
   // ===== legal notes =====
-  addHtml("hr");
-  addHtml("div", "<p class='small'>The puzzle game 'madeofnumbers/10' and its editor '10 / MAKER' are &copy;2024 IOJOE / madeofnumbers. All rights reserved.<br>'10 / CODEPLAY' examples are open-source under the MIT license &copy;2024 madeofnumbers</p>")
+  addElement("hr");
+  addElement("div", "<p class='small'>The puzzle game 'madeofnumbers/10' and its editor '10 / MAKER' are &copy;2024&nbsp;IOJOE&nbsp;/&nbsp;madeofnumbers. All&nbsp;rights&nbsp;reserved.<br>'10 / CODEPLAY' examples are open-source under the MIT&nbsp;license &copy;2024&nbsp;madeofnumbers<br><br><br></p>")
 
 
   /* ========================================================================== */
 
 
   // ===== add dom elements =====
-  function addHtml(type, html, id) {
+  function addElement(type, html, id) {
 
     let elAdd = document.createElement(type);
     if (html) {
@@ -257,7 +259,7 @@ function addCodeplayExamplesDemo(domElement) {
 
     if (example.use) {
       let elUse = document.createElement("button");
-      elUse.innerHTML = "⮐";
+      elUse.innerHTML = "CALL";
       elUse.onclick = () => {
         elResult.innerHTML = example.use();
       }
