e91a1e60f17f06ef38b29dfe9ca5d88dbf511125
Simon <simon@madeofnumbers.com>
2024-03-30 12:32:05 +0100

    Update demo.js

--- demo/demo.js
+++ demo/demo.js
@@ -16,8 +16,8 @@ function addCodeplayExamplesDemo(domElement) {
 
   // ===== notes on 10 / CODEPLAY =====
   addElement("h1", "10 / CODEPLAY : write code to solve puzzles");
-  addElement("div", "This examples demo is not great on small&nbsp;screens. We would recommend you try it out on a larger&nbsp;display.", "note-small-screens");
-  addElement("p", "<strong>GitHub : <a href='https://github.com/madeofnumbers/10-codeplay/' target='_blank'>https://github.com/madeofnumbers/10-codeplay/</a></strong>");
+  addElement("div", "This examples demo is not very user friendly on small&nbsp;screens. We would recommend you try it out on a larger&nbsp;display.", "note-small-screens");
+  addElement("p", "<strong>GitHub : <a href='https://github.com/madeofnumbers/10-codeplay/'>https://github.com/madeofnumbers/10-codeplay/</a></strong>");
   addElement("p", "<strong>About :</strong> example code to interact with <a href='https://madeofnumbers.com/10/' target='_blank'>madeofnumbers/10</a>, a game where the player slides numbers into each other to make 10s. Once all the numbers have been combined, the player has solved the&nbsp;puzzle.");
   addElement("p", "<a href='#example-grid-summary'>Example : gridSummary()</a><br><a href='#example-random-moves'>Example : randomMoves()</a>")
   addElement("p", "The <strong>Mon10</strong> object from <strong>https://madeofnumbers.com/10/mon10-latest.min.js</strong> provides the following&nbsp;methods :");
@@ -151,26 +151,27 @@ function addCodeplayExamplesDemo(domElement) {
       value: "3MpDvCvDpM"
     }],
     use: () => {
-      let makerCustom = getValue("makerCustom");
-      if (makerCustom.split("c=").length > 1) {
-        custom = custom.split("?c=")[1];
+      let custom = getValue("makerCustom");
+      // ===== parse custom string to support copy/paste of entire share link from 10 / MAKER =====
+      if (custom.split("c=").length > 1) {
+        custom = custom.split("c=")[1].split("?")[0];
       }
-      mon10.loadMaker(makerCustom);
+      mon10.loadMaker(custom);
       return ""
     },
     comment: "load a custom 10 / MAKER puzzle"
   });
 
   // ===== notes on Mon10 object =====
-  addElement("p", "<strong>mon10.getXY(x, y)</strong><br> -- <strong>x, y</strong> // (int, int) position : top-left corner of the grid is (0, 0)<br>result :<br> -- <strong>block: 1</strong> // is a non-numeric block tile<br> -- <strong>empty: 1</strong> // is empty, no tile at that position<br> -- <strong>number: <em>value</em></strong> // is a numeric tile, including <strong>number: 0</strong> which is 0<br> -- <strong>move: <em>direction</em></strong> // permitted directions : 0 = none, 1 = up, 2 = right, 3 = down, 4 = left, 5 = up/down, 6 = left/right, 7 = all<br>");
-  addElement("p", "<strong>mon10.moveXYD(x, y, d)</strong><br> -- <strong>x, y</strong> // (int, int) position : top-left corner of the grid is (0, 0)<br> -- <strong>d</strong> // (int) direction : 1 = up, 2 = right, 3 = down, 4 = left<br>result :<br> -- <strong>x:, y:</strong> // tile moved to this new position<br> -- <strong>fail: 1</strong> // tile could not be moved<br> -- <strong>fail: 2</strong> // tile could have been moved but would result in an illegal value (below -10 or above 10)<br>");
-  addElement("p", "<strong>mon10.loadMaker(c)</strong><br> -- <strong>c</strong> // (string) custom : for test purposes you can load puzzles created in <a href='https://madeofnumbers.com/10/maker/' target='_blank'>10 / MAKER</a><br>In a share link the parameter '<strong>c=</strong>' is the custom string - e.g. from <a href='https://madeofnumbers.com/10/?c=3MpDvCvDpM' target='_blank'>https://madeofnumbers.com/10/?c=3MpDvCvDpM</a> the custom string is&nbsp;<strong>3MpDvCvDpM</strong> - an empty or invalid string will result in the daily puzzle being loaded");
+  addElement("p", "<strong>mon10.getXY(x, y)</strong><br><ul class='input'><li><strong>x, y</strong> <em>// (int, int) position : top-left corner of the grid is (0, 0)</em></li></ul>result :<strong><ul class='result'><li>block: 1</strong> <em>// is a non-numeric block tile</em></li><li><strong>empty: 1</strong> <em>// is empty, no tile at that position</em></li><li><strong>number:</strong> value <em>// (int) numeric tile, including 'number: 0' which is value 0</em></li><li><strong>move:</strong> direction <em>// (int) permitted directions : 0 = none, 1 = up, 2 = right, 3 = down, 4 = left, 5 = up/down, 6 = left/right, 7 = all</em></li></ul>");
+  addElement("p", "<strong>mon10.moveXYD(x, y, d)</strong><ul class='input'><li><strong>x, y</strong> <em>// (int, int) position : top-left corner of the grid is (0, 0)</em></li><li><strong>d</strong> <em>// (int) direction : 1 = up, 2 = right, 3 = down, 4 = left</em></li></ul>result :<ul class='result'><li><strong>x:, y:</strong> <em>// (int, int) tile moved to this new position</em></li><li><strong>fail: 1</strong> <em>// tile could not be moved</em></li><li><strong>fail: 2</strong> <em>// tile could be moved but would result in an illegal value below -10 or above 10</em></li></ul>");
+  addElement("p", "<strong>mon10.loadMaker(c)</strong><ul class='input'><li><strong>c</strong> <em>// (string) custom : for test purposes you can load puzzles created in <a href='https://madeofnumbers.com/10/maker/' target='_blank'>10 / MAKER</a><br>From a share link the parameter '<strong>c=</strong>' is the custom string - e.g. from <a href='https://madeofnumbers.com/10/?c=3MpDvCvDpM' target='_blank'>https://madeofnumbers.com/10/?c=3MpDvCvDpM</a> the custom string is&nbsp;<strong>3MpDvCvDpM</strong> - an empty or invalid string will result in the daily puzzle being loaded</em></li></ul>");
   
   // ===== example gridSummary() / mon10-codeplay-example-grid-summary.js =====
   addElement("hr", "", "example-grid-summary");
   addElement("h1", "Example : gridSummary()");
   addElement("p", "Uses mon10.getXY() to build a simple summary of the current grid state, including positions of tiles grouped by&nbsp;value");
-  addElement("p", "Script : <a href='../examples/grid-summary/mon10-codeplay-example-grid-summary.js' target='_blank'>examples/grid-summary/mon10-codeplay-example-grid-summary.js</a>");
+  addElement("p", "Script : <a href='https://github.com/madeofnumbers/10-codeplay/blob/main/examples/grid-summary/mon10-codeplay-example-grid-summary.js'>examples/grid-summary/mon10-codeplay-example-grid-summary.js</a>");
   addElement("p", "", "html-grid-summary");
   addExample({
     name: "gridSummary",
@@ -185,7 +186,7 @@ function addCodeplayExamplesDemo(domElement) {
   addElement("hr", "", "example-random-moves");
   addElement("h1", "Example : randomMoves()");
   addElement("p", "Uses mon10.getXY(), mon10.moveXYD(), mon10.nonzero() and mon10.size() to make entirely random moves with almost no chance of solving&nbsp;a&nbsp;puzzle");
-  addElement("p", "Script : <a href='../examples/random-moves/mon10-codeplay-example-random-moves.js' target='_blank'>examples/random-moves/mon10-codeplay-example-random-moves.js</a>");
+  addElement("p", "Script : <a href='https://github.com/madeofnumbers/10-codeplay/blob/main/examples/random-moves/mon10-codeplay-example-random-moves.js'>examples/random-moves/mon10-codeplay-example-random-moves.js</a>");
   addElement("p", "", "html-random-moves-log");
   addExample({
     name: "randomMoves",
@@ -261,7 +262,10 @@ function addCodeplayExamplesDemo(domElement) {
       let elUse = document.createElement("button");
       elUse.innerHTML = "CALL";
       elUse.onclick = () => {
-        elResult.innerHTML = example.use();
+        let result = example.use();
+        if(result) {
+          elResult.innerHTML = example.use();
+        }
       }
       elExample.appendChild(elUse);
     }
